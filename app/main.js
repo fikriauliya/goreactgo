2c828886c19ac07782a5da1ba52c7070f84ca550
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2015-10-11 22:06:58 +0700

    Add react-hot loader

--- app/main.js
+++ app/main.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './component.jsx';
 
-main();
+var rootInstance = ReactDOM.render(<Hello />, document.getElementById('app'));
 
-function main() {
-    ReactDOM.render(<Hello />, document.getElementById('app'));
+if (module.hot) {
+  require('react-hot-loader/Injection').RootInstanceProvider.injectProvider({
+    getRootInstances: function () {
+      // Help React Hot Loader figure out the root component instances on the page:
+      return [rootInstance];
+    }
+  });
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ var path = require('path');
 
 module.exports = {
     entry: [
+      'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
+      'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
       path.resolve(__dirname, 'app/main.js')
     ],
     output: {
@@ -12,7 +14,7 @@ module.exports = {
       loaders: [{
         test: /\.jsx?$/, // A regexp to test the require path. accepts either js or jsx
         exclude: /node_modules/,
-        loader: 'babel' // The module to load. "babel" is short for "babel-loader"
+        loaders: ['react-hot', 'babel'] // The module to load. "babel" is short for "babel-loader"
       }, {
         test: /\.css$/, // Only .css files
         loader: 'style!css' // Run both loaders
