ed3494afb989a992b85975332a51fe128efc147e
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2015-10-11 21:58:23 +0700

    Add webpack css & style loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,17 @@ module.exports = {
       path.resolve(__dirname, 'app/main.js')
     ],
     output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.js',
     },
     module: {
       loaders: [{
         test: /\.jsx?$/, // A regexp to test the require path. accepts either js or jsx
         exclude: /node_modules/,
         loader: 'babel' // The module to load. "babel" is short for "babel-loader"
+      }, {
+        test: /\.css$/, // Only .css files
+        loader: 'style!css' // Run both loaders
       }]
     }
 };
